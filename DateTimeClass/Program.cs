4ce43c47a6a4a277cdb4dae29a7b6059f493f444
strdos <81235866+strdos@users.noreply.github.com>
2022-06-07 23:27:29 +0300

    add DateTime and nullable examples

---
+++ DateTimeClass/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DateTimeClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime(2022, 6, 7);
+            Console.WriteLine(dateTime);
+            Console.WriteLine(DateTime.Today);
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now - dateTime);
+            Console.WriteLine(GetTomorrow());
+            Console.WriteLine(DateTime.Today.DayOfWeek);
+            Console.WriteLine(GetFirstDayOfYear(2022));
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Current minute is: {0}", now.Minute);
+            Console.WriteLine("Now is: {0} o'clock, {1} minutes, and {2} seconds.", now.Hour, now.Minute, now.Second);
+
+            // calculates how many days have passed since a date entered by the user
+            Console.WriteLine("Enter a date in this format: yyyy-mm-dd");
+            string input = Console.ReadLine();
+            if(DateTime.TryParse(input, out dateTime))
+            {
+                Console.WriteLine(dateTime);
+                TimeSpan daysPassed = now - dateTime; // or TimeSpan daysPassed = now.Subtract(dateTime);
+                Console.WriteLine(daysPassed.Days);
+            }
+        }
+        static DateTime GetTomorrow()
+        {
+            return DateTime.Today.AddDays(1);
+        }
+        static DayOfWeek GetFirstDayOfYear(int year)
+        {
+            DateTime dateTime = new DateTime(year, 1, 1);
+            return dateTime.DayOfWeek;
+        }
+    }
+}
---
+++ Nullables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Nullables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ways to declare and assign nullables
+            int? num1 = null;
+            int? num2 = 1337;
+            double? num3 = new double?();
+            double? num4 = 3.1415;
+            bool? boolVal = new bool?();
+
+            Console.WriteLine("Our nullable data is: {0}, {1}, {2}, {3}, and {4}", num1, num2, num3, num4, boolVal);
+
+            double? num5 = null;
+            double num6 = 4.1245;
+            double num7 = num5 ?? num6; // ?? null coalescing operator checks if a value to the left is null
+            Console.WriteLine(num7);
+        }
+    }
+}
