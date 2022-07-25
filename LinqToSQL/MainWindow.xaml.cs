84071a5fe8ddc44cefaa9e3724265a3757c2ce8a
strdos <81235866+strdos@users.noreply.github.com>
2022-07-25 22:15:58 +0300

    add LINQ to SQL class example

--- LinqToSQL/MainWindow.xaml.cs
+++ LinqToSQL/MainWindow.xaml.cs
@@ -29,18 +29,94 @@ namespace LinqToSQL
             string connectionString = ConfigurationManager.ConnectionStrings["LinqToSQL.Properties.Settings.UdemyDBConnectionString"].ConnectionString;
             dataContext = new LinqToSqlDataClassesDataContext(connectionString);
 
-            InsertUniversities();
+            //InsertUniversities();
+            //InsertStudents();
+            //InsertLectures();
+            //InsertStudentLectureAssociations();
+            //GetUniversityOfToni();
+            //GetLecturesOfToni();
+            GetAllStudentsFromYale();
         }
 
         public void InsertUniversities()
         {
+            dataContext.ExecuteCommand("delete from University");
+            
             University yale = new University();
             yale.Name = "Yale";
             dataContext.Universities.InsertOnSubmit(yale);
+
+            University beijingTech = new University();
+            beijingTech.Name = "Beijing Tech";
+            dataContext.Universities.InsertOnSubmit(beijingTech);
 
             dataContext.SubmitChanges();
 
             MainDataGrid.ItemsSource = dataContext.Universities;
         }
+        public void InsertStudents()
+        {
+            dataContext.ExecuteCommand("delete from Student");
+
+            University yale = dataContext.Universities.First(un => un.Name.Equals("Yale")); // returns instance of the object
+            // ... "from university in dataContext.University where university == "Yale" select university" returns Collection (IEnumerable)
+            University beijingTech = dataContext.Universities.First(un => un.Name.Equals("Beijing Tech"));
+
+            List<Student> students = new List<Student>();
+            students.Add(new Student { Name = "Carla", Gender = "female", UniversityId = yale.Id});
+            students.Add(new Student { Name = "Toni", Gender = "male", University = yale });
+            students.Add(new Student { Name = "Lea", Gender = "female", University = beijingTech });
+
+            dataContext.Students.InsertAllOnSubmit(students);
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Students;
+        }
+        public void InsertLectures()
+        {
+            dataContext.Lectures.InsertOnSubmit(new Lecture { Name = "Math" });
+            dataContext.Lectures.InsertOnSubmit(new Lecture { Name = "History" });
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Lectures;
+        }
+        public void InsertStudentLectureAssociations()
+        {
+            Student Carla = dataContext.Students.First(st => st.Name.Equals("Carla"));
+            Student Toni = dataContext.Students.First(st => st.Name.Equals("Toni"));
+            Student Lea = dataContext.Students.First(st => st.Name.Equals("Lea"));
+
+            Lecture Math = dataContext.Lectures.First(lc => lc.Name.Equals("Math"));
+            Lecture History = dataContext.Lectures.First(lc => lc.Name.Equals("History"));
+
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = Carla, Lecture = Math });
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = Toni, Lecture = Math });
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = Lea, Lecture = History });
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.StudentLectures;
+        }
+        public void GetUniversityOfToni()
+        {
+            Student Toni = dataContext.Students.First(st => st.Name.Equals("Toni"));
+            University TonisUniversity = Toni.University;
+
+            List<University> universities = new List<University>();
+            universities.Add(TonisUniversity);
+            MainDataGrid.ItemsSource = universities;
+        }
+        public void GetLecturesOfToni()
+        {
+            Student Toni = dataContext.Students.First(st => st.Name.Equals("Toni"));
+            var tonisLectures = from sl in Toni.StudentLectures select sl.Lecture;
+            MainDataGrid.ItemsSource = tonisLectures;
+        }
+        public void GetAllStudentsFromYale()
+        {
+            var studentsFromYale = from student in dataContext.Students where student.University.Name == "Yale" select student;
+            MainDataGrid.ItemsSource = studentsFromYale;
+        }        
     }
 }
